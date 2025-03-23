268170dab66cfb18752ccc3fb3b6a2e171cb608d
mkashif341 <IQR@university123>
2025-03-24 02:30:04 +0500

    Add project files.

---
+++ JazzCashDashboardFrontApp/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JazzCashDashboardFrontApp.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult home()
+        {
+            return View();
+        }
+
+        public IActionResult analytics()
+        {
+            return View();
+        }
+    }
+}
